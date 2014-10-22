aa587cc597fc8150c8d4a980751c728bd4a74991
Eben Freeman <eben@inboxapp.com>
2014-10-22 15:14:29 -0700

    Reduce test contact sync poll frequency.

--- tests/util/base.py
+++ tests/util/base.py
@@ -252,7 +252,7 @@ def default_account(db):
 @fixture(scope='function')
 def contact_sync(config, db):
     from inbox.contacts.remote_sync import ContactSync
-    return ContactSync('gmail', 1, 1)
+    return ContactSync('gmail', 1, 1, poll_frequency=0)
 
 
 @fixture(scope='function')
